b4c3ded4e2608dfca4f4b2ece9921093eaa1e5b5
Álvaro Castro Sánchez <alvaro.castro@iesnervion.es>
2022-11-17 19:13:52 +0100

    empezando el 3

---
+++ _02Ejercicio3/src/GestorHojas.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+public class GestorHojas extends Thread {
+
+	private static  List<String> lista = Collections.synchronizedList(new ArrayList<>());
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			lista.add(new String("Texto" + i));
+		}
+
+		for (String string : lista) {
+			System.out.println(string);
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			new GestorHojas().start();
+		}
+
+
+	}
+
+
+}
