5ac31876310e5314a3c79226050f8eb915ebef6e
Álvaro Castro Sánchez <alvaro.castro@iesnervion.es>
2022-11-17 21:30:11 +0100

    terminado el ejercicio 3

--- _02Ejercicio3/src/GestorHojas.java
+++ _02Ejercicio3/src/GestorHojas.java
@@ -2,24 +2,28 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GestorHojas extends Thread {
 
-	private static  List<String> lista = Collections.synchronizedList(new ArrayList<>());
+	//Cambiamos el arrayList por una lista en la que acceden los hilos de uno en uno
+	private volatile static CopyOnWriteArrayList<String> lista = new CopyOnWriteArrayList<String>();
 
 	@Override
 	public void run() {
+		//Añadimos el texto al array
 		for (int i = 0; i < 10; i++) {
 			lista.add(new String("Texto" + i));
 		}
 
+		//Recorre el array
 		for (String string : lista) {
 			System.out.println(string);
 		}
 	}
 
 	public static void main(String[] args) {
+		//Creamos e iniciamos los hilos
 		for (int i = 0; i < 10; i++) {
 			new GestorHojas().start();
 		}
